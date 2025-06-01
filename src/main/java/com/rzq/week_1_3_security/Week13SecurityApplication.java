d11717d112d69661e6064fe84645ba4044271882
mkhoiqi <mkhoirurrizqi08@gmail.com>
2025-06-01 23:25:06 +0700

    first commit

---
+++ src/main/java/com/rzq/week_1_3_security/Week13SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.rzq.week_1_3_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week13SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week13SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rzq/week_1_3_security/Week13SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzq.week_1_3_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week13SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
